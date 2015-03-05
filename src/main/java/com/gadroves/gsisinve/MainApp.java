db94533be1ae2fbcbfa71fae61994b816cbc0d7d
geyrix57 <geyrix57@hotmail.com>
2015-03-04 22:10:26 -0600

    ...

--- src/main/java/com/gadroves/gsisinve/MainApp.java
+++ src/main/java/com/gadroves/gsisinve/MainApp.java
@@ -3,27 +3,31 @@ package com.gadroves.gsisinve;
  * Created by geykel on 04/03/2015.
  */
 
-import com.gadroves.gsisinve.model.beans.Articulo;
-import com.gadroves.gsisinve.model.beans.Bodega;
-import com.gadroves.gsisinve.model.daos.ArticuloDAO;
+import com.gadroves.gsisinve.controller.LoginUserController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 public class MainApp extends Application {
 
     public static void main(String[] args) {
-        ArrayList<Bodega> arrayList = new ArrayList<>();
-        arrayList.add(new Bodega("", "1"));
-        List<Articulo> articulos = ArticuloDAO.getInstance().select().AllInBodegaList(arrayList);
-        for (Articulo ar : articulos) System.out.println(ar.toString());
-        //launch(args);
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("views/fxml/LoginUser.fxml"));
+        primaryStage.setScene(new Scene((Pane) loader.load()));
+        primaryStage.setResizable(false);
+        loader.<LoginUserController>getController().initData(primaryStage);
+        /*ArrayList<Bodega> arrayList = new ArrayList<>();
+        arrayList.add(new Bodega("", "1"));
+        List<Articulo> articulos = ArticuloDAO.getInstance().select().AllInBodegaList(arrayList);
+        for (Articulo ar : articulos) Mensajes.InformationDialog(ar.getCodigo() + " " + ar.getDescripcion(), primaryStage);*/
+        primaryStage.show();
     }
 }
