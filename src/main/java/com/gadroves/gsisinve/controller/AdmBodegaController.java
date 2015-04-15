4e18da89a9de7c2a0bc1f4e9271aba559e58fb71
josanuz <josanuz@hotmail.es>
2015-04-14 21:09:07 -0600

    Terminado AdmBodegas

--- src/main/java/com/gadroves/gsisinve/controller/AdmBodegaController.java
+++ src/main/java/com/gadroves/gsisinve/controller/AdmBodegaController.java
@@ -2,6 +2,7 @@ package com.gadroves.gsisinve.controller;
 
 import com.gadroves.gsisinve.model.DBAccess;
 import com.gadroves.gsisinve.model.entities.TbBodega;
+import com.gadroves.gsisinve.utils.DialogBox;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +16,7 @@ import java.util.ResourceBundle;
  */
 public class AdmBodegaController implements Initializable {
     boolean update = false;
+    TbBodega refered = null;
 
     @FXML
     TextField TF_Bodega_desc;
@@ -29,9 +31,11 @@ public class AdmBodegaController implements Initializable {
                 TbBodega ref = dbAccess.Stream(TbBodega.class).where(tb->tb.getCode().equals(s)).getOnlyValue();
                 TF_Bodega_desc.setText(ref.getDesc());
                 update = true;
+                refered = ref;
             }catch (Exception es){
                 TF_Bodega_desc.setText("");
                 update = false;
+                refered = null;
             }
         };
         TF_Bodega_ID.setOnAction(rr);
@@ -43,11 +47,45 @@ public class AdmBodegaController implements Initializable {
                     TbBodega ref = dbAccess.Stream(TbBodega.class).where(tb->tb.getCode().equals(s)).getOnlyValue();
                     TF_Bodega_desc.setText(ref.getDesc());
                     update = true;
+                    refered = ref;
                 }catch (Exception es){
                     TF_Bodega_desc.setText("");
                     update = false;
+                    refered = null;
                 }
             }
         });
+    }
+    @FXML
+    private void guardar() throws Exception {
+        DBAccess dbAccess = DBAccess.getInstance();
+        if(update) {
+            dbAccess.getTransaction().begin();
+            refered.setDesc(TF_Bodega_desc.getText());
+            dbAccess.Update(refered);
+            dbAccess.getTransaction().commit();
+        }
+        else{
+            dbAccess.getTransaction().begin();
+            TbBodega tb = new TbBodega();
+            tb.setCode(TF_Bodega_ID.getText());
+            tb.setDesc(TF_Bodega_desc.getText());
+            dbAccess.Insert(tb);
+            dbAccess.getTransaction().commit();
+            refered = tb;
+            update = true;
+        }
+    }
+    @FXML
+    private void eliminarBD() throws Exception{
+        DBAccess dbAccess = DBAccess.getInstance();
+        if(refered != null) {
+            dbAccess.getTransaction().begin();
+            dbAccess.Delete(refered);
+            dbAccess.getTransaction().commit();
+            TF_Bodega_ID.setText("");
+            TF_Bodega_desc.setText("");
+        }
+        else DialogBox.Error(null,"No hay ningun Objeto Seleccionado");
     }
 }
